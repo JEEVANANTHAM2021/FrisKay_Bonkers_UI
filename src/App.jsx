e174db61bbb7dba53f60309906142231d922b9a4
JEEVANANTHAM2021 <jeevanathamjeeva64@gmail.com>
2025-12-21 13:05:52 +0530

    Updated files

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import WhereToBuy from './pages/WhereToBuy'
 import Contact from './pages/Contact'
 import {Routes, Route} from 'react-router-dom'
 import FaqData from './pages/FaqData'
+import OurTreats from './pages/OurTreats'
 
 const App = () => {
   return (
     <div className='w-full'>
       <Routes>
         <Route path= "/" element={<Home />} />
+        <Route path='/treats' element={<OurTreats />} />
         <Route path='/buy' element={<WhereToBuy />} />
         <Route path='/FAQ' element={<FaqData />} />
         <Route path='/contact' element={<Contact />} />
