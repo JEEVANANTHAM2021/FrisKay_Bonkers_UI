bede31cca11d240963d8b04e57eee5fc3f3272c7
JEEVANANTHAM2021 <jeevanathamjeeva64@gmail.com>
2025-12-27 09:45:46 +0530

    Updated files

--- src/components/PageLoader.jsx
+++ src/components/PageLoader.jsx
@@ -1,16 +1,21 @@
-import load_img from "../assets/images/load_img.png";
+import React from "react";
+import { assets } from "../assets/assets";
 
 const PageLoader = () => {
   return (
     <div
-      className="w-full min-h-screen flex justify-center items-center bg-[#F7E2AC]"
+      className="w-full min-h-screen flex justify-center items-center bg-[#FFEBC3]"
       style={{
-        backgroundImage: `url(${load_img})`,
-        backgroundSize: "cover",       
-        backgroundPosition: "center", 
-        backgroundRepeat: "no-repeat"
+        backgroundImage: `${assets.load_img}`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        filter: "brightness(0.92)"
       }}
     >
+      <h1 className="text-5xl md:text-8xl font-['Dinosaur'] text-black opacity-70 tracking-widest -rotate-30 ">
+        BONKER’S
+      </h1>
     </div>
   );
 };
